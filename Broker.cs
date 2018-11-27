04bf9f2e74e87b064d17b204eb07ca21ff0602c8
Danny <danielhogg@protonmail.com>
2018-11-26 19:35:02 -0500

    added DefaultStatusLabel field to config, finalized v1.0.1

--- Broker.cs
+++ Broker.cs
@@ -18,22 +18,6 @@ namespace Marksman
 
         }
 
-        /*
-        public static void testCreate(SnipeItApi snipe)
-        
-            // Get all information
-
-            // Create all asset, model, 
-
-            SnipeSharp.Endpoints.Models.Manufacturer currentManufacturer = new SnipeSharp.Endpoints.Models.Manufacturer
-            {
-                Name = "PCIExpress"
-            };
-
-            var manufacturerResponse = snipe.ManufacturerManager.Create(currentManufacturer);  
-        }
-        */
-
         public List<IRequestResponse> SyncAll(SnipeItApi snipe, Asset currentAsset, Model currentModel, Manufacturer currentManufacturer,
             Category currentCategory, Company currentCompany, StatusLabel currentStatusLabel, Location currentLocation)
         {
@@ -77,36 +61,5 @@ namespace Marksman
 
             return messages;
         }
-
-        /*
-        public static void syncAsset(SnipeItApi snipe, SnipeSharp.Endpoints.Models.Asset currentAsset)
-        {
-            try
-            {
-                SnipeSharp.Endpoints.SearchFilters.SearchFilter assetFilter = new SnipeSharp.Endpoints.SearchFilters.SearchFilter()
-                {
-                    Search = currentAsset.AssetTag
-                };
-
-                SnipeSharp.Endpoints.Models.Asset foundAsset = snipe.AssetManager.FindOne(assetFilter);
-                if (foundAsset != null && foundAsset.AssetTag == currentAsset.AssetTag)
-                {
-                    Trace.WriteLine("Asset already exists in db. Not added.");
-                }
-                else
-                {
-                    var response = snipe.AssetManager.Create(currentAsset);
-                    Trace.WriteLine("Response recieved from SnipeIT server after attempting to add asset: ");
-                    Trace.WriteLine(response);
-                }
-            } catch (Exception e)
-            {
-                Trace.WriteLine("Exception encountered while adding asset: " + e.ToString());
-            }
-
-        }
-        */
     }
-
-
 }
